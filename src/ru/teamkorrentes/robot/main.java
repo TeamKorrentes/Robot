93a57de2e740278b938d089f14bddd0b6d1a2078
Alexandr <naplle58@gmail.com>
2016-10-31 20:21:52 +0300

    ща будем баги чинить

--- src/ru/teamkorrentes/robot/main.java
+++ src/ru/teamkorrentes/robot/main.java
@@ -3,10 +3,12 @@ package ru.teamkorrentes.robot;
 public class main{
     public static void main(String[] args) {
         Point start = new Point(0, 0);
-        Point end = new Point(-15.5, -10.6);
+        Point end = new Point(-15, -10);
 
-        Robot robot = new Robot();
+        Robot robot = new Robot(start);
         robot.move(end);
+        System.out.println(robot.toString());
+        robot.move(start);
         System.out.println(robot.toString());
     }
 }
